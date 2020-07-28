59aec4a043496a33c124b041d17e16792868dd30
Mohammad Burhani <35170510+mdburhani52@users.noreply.github.com>
2020-07-28 12:48:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,38 +1,86 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import ScrollToTop from 'react-router-scroll-top';
 import { bindActionCreators } from 'redux';
-import {getData} from 'store/actions';
+import { getData } from 'store/actions';
 import logo from './logo.svg';
 import './App.css';
 
 
-class App extends Component {
+function App(props) {
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1>{this.props.title}</h1>
-          <button onClick={()=>this.props.getData()}>Click me and check network</button>
-
-          {
-            this.props.data1 !=null?
-          <div>Id:-{this.props.data1.id} <br/> Created At:-{this.props.data1.createdAt}</div>:null
-          }
-        </header>
-      </div>
+  const [count, setCount] = useState(0);
+  const [myVal, setmyVal] = useState('');
+  const [todos, setTodos] = useState(
+    [
+      { name: 'James' },
+      { name: 'Dany' },
+      { name: 'Micheal' }
+    ]
     );
+
+  function acx() {
+    setCount(count+1)
   }
+
+  function changeHandler(e){
+    setmyVal(e.target.value);
+  }
+
+  function arrChangeHandler(){
+    let ab = todos;
+    ab[0].name = "mohammad burhani";
+   
+    setTodos([...ab]);
+  }
+  
+  useEffect(() => {
+    console.log(count)
+  });
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>{props.title}</h1>
+        <button onClick={() => props.getData()}>Click me and check network</button>
+        <br/>
+        {
+          props.data1 != null ?
+            <div>Id:-{props.data1.id} <br /> Created At:-{props.data1.createdAt}</div> : null
+        }
+        <br/>
+
+        <button onClick={acx}>Click me  {count}</button>
+        <br/>
+
+        <input 
+          type={'text'} 
+          name={'myVal'}
+          value={myVal}
+          onChange={changeHandler}
+        />
+        <br/>
+
+        {
+          todos.map((tod, i) => {
+          return(<div key={i}>{tod.name}</div>)
+          })
+        }
+        <button onClick={arrChangeHandler}>Change Name</button>
+
+      </header>
+    </div>
+  );
+
 };
 
 
 const mapStateToProps = state => {
   return {
     title: state.user.Reducer1.title,
-    data1 :state.user.Reducer1.data1
+    data1: state.user.Reducer1.data1
   };
 };
 
